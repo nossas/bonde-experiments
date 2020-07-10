1bd2ce4262b2cf53bce176fb9975314c34c343aa
Igor Santos <igr.exe@gmail.com>
2020-07-10 15:11:30 -0300

    chore(activists): add mailchimp function tests

---
+++ packages/bonde-activists/src/mailchimp.spec.ts
@@ -0,0 +1,167 @@
+import { mocked } from 'ts-jest/utils';
+import Mailchimp from 'mailchimp-api-v3';
+import crypto from 'crypto';
+import { Activist, Widget } from './types';
+import mailchimp, { tags, hash } from './mailchimp';
+
+const mockPut = jest.fn();
+const mockPost = jest.fn();
+jest.mock('mailchimp-api-v3', () =>
+  jest.fn().mockImplementation(() => {
+    return {
+      put: mockPut,
+      post: mockPost
+    }
+  })
+);
+mocked(Mailchimp, true);
+
+describe('mailchimp function tests', () => {
+  const activist: Activist = {
+    id: 345,
+    email: 'activist@test.org',
+    name: 'Activist Name'
+  }
+  const widget: Widget = {
+    id: 38,
+    kind: 'pressure',
+    settings: {},
+    block: {
+      mobilization: {
+        id: 6,
+        community: {
+          id: 1,
+          mailchimp_api_key: 'xxx-us10',
+          mailchimp_list_id: 'xxx'
+        }
+      }
+    }
+  }
+
+  it('hash function should be return a md5 of email with lower case', () => {
+    const email = 'Activist@test.org';
+    expect(hash(email)).toEqual(
+      crypto
+        .createHash('md5')
+        .update(email.toLowerCase())
+        .digest('hex')
+    );
+  });
+
+  it('tags function should be return a COMMUNITY, MOBILIZATION and ACTION tags', () => {
+    const { id, kind, block: { mobilization } } = widget;
+    const expected = [
+      'C' + mobilization.community.id,
+      'M' + mobilization.id,
+      kind.toUpperCase().substring(0, 1) + '' + id
+    ];
+
+    expect(tags(widget)).toEqual(expected);
+  });
+
+  it('subscribe Mailchimp put an activist', async () => {
+    mockPut.mockResolvedValue({ last_changed: '' });
+
+    const { mailchimp_list_id } = widget.block.mobilization.community
+    const activistEntity = {
+      ...activist,
+      first_name: 'Activist',
+      last_name: 'Name'
+    }
+    const expected = {
+      path: `/lists/${mailchimp_list_id}/members/${hash(activist.email)}`,
+      body: {
+        email_address: activistEntity.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: activistEntity.first_name,
+          LNAME: activistEntity.last_name
+        },
+        tags: tags(widget)
+      }
+    };
+    const { subscribe } = mailchimp({ widget, activist: activistEntity });
+    
+    return subscribe()
+      .then(() => {
+        expect(mockPut).toBeCalledWith(expected);
+      });
+  });
+
+  it('subscribe Mailchimp merge_fields activist with name by default', async () => {
+    mockPut.mockResolvedValue({ last_changed: '' });
+
+    const { mailchimp_list_id } = widget.block.mobilization.community
+    const expected = {
+      path: `/lists/${mailchimp_list_id}/members/${hash(activist.email)}`,
+      body: {
+        email_address: activist.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: activist.name,
+          LNAME: activist.name
+        },
+        tags: tags(widget)
+      }
+    };
+    const { subscribe } = mailchimp({ widget, activist });
+
+    return subscribe()
+      .then(() => {
+        expect(mockPut).toBeCalledWith(expected);
+      });
+  });
+
+  it('subscribe Mailchimp merge_fields activist like a city or phone', async () => {
+    mockPut.mockResolvedValue({ last_changed: '' });
+
+    const activistEntity = {
+      ...activist,
+      city: 'Rio de Janeiro',
+      phone: '021999999999'
+    };
+    const { mailchimp_list_id } = widget.block.mobilization.community;
+    const expected = {
+      path: `/lists/${mailchimp_list_id}/members/${hash(activistEntity.email)}`,
+      body: {
+        email_address: activist.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: activistEntity.name,
+          LNAME: activistEntity.name,
+          CITY: activistEntity.city,
+          PHONE: activistEntity.phone
+        },
+        tags: tags(widget)
+      }
+    };
+    const { subscribe } = mailchimp({ widget, activist: activistEntity });
+
+    return subscribe()
+      .then(() => {
+        expect(mockPut).toBeCalledWith(expected);
+      });
+  });
+
+  it('merge Mailchimp to create extra fields settings', async () => {
+    mockPost.mockResolvedValue({});
+
+    const { mailchimp_list_id } = widget.block.mobilization.community;
+    const expected = {
+      path: `/lists/${mailchimp_list_id}/merge-fields`,
+      body: {
+        tag: 'CITY',
+        name: "City",
+        type: 'text',
+        required: false,
+        public: true
+      }
+    };
+    const { merge } = mailchimp({ widget, activist });
+
+    return merge()
+      .then(() => {
+        expect(mockPost).toBeCalledWith(expected);
+      });
+  });
+});
--- packages/bonde-activists/src/mailchimp.ts
+++ packages/bonde-activists/src/mailchimp.ts
@@ -12,6 +12,24 @@ interface MailchimpResolve {
   merge: () => Promise<any>
 }
 
+export const tags = (widget: Widget) => {
+  const { id, kind, block: { mobilization } } = widget;
+  return [
+    // TAG COMMUNITY
+    'C' + mobilization.community.id,
+    // TAG MOBILIZATION
+    'M' + mobilization.id,
+    // TAG WIDGET KIND
+    kind.toUpperCase().substring(0, 1) + '' + id
+  ];
+};
+
+export const hash = (email: string) => crypto
+  .createHash('md5')
+  .update(email.toLowerCase())
+  .digest('hex')
+;
+
 export default <T extends Widget, U extends Activist>(args: MailchimpArgs<T, U>): MailchimpResolve => {
   // Properties
   const { activist, widget } = args;
@@ -23,48 +41,28 @@ export default <T extends Widget, U extends Activist>(args: MailchimpArgs<T, U>)
   const client = new Mailchimp(mailchimp_api_key);
   const listID = mailchimp_list_id;
 
-  // Utils functions
-  const tags = () => {
-    const { id, kind, block: { mobilization } } = widget;
-    return [
-      // TAG COMMUNITY
-      'C' + mobilization.community.id,
-      // TAG MOBILIZATION
-      'M' + mobilization.id,
-      // TAG WIDGET KIND
-      kind.toUpperCase().substring(0,1) + '' + id
-    ]
-  };
-  const hash = () => crypto
-    .createHash('md5')
-    .update(activist.email.toLowerCase())
-    .digest('hex')
-  ;
-
   // Mailchimp Functions
   return {
     subscribe: async (): Promise<any> => {
-      let form: any = {
+      let body: any = {
         "email_address": activist.email,
         "status": "subscribed",
         "merge_fields": {
           "FNAME": activist.first_name || activist.name,
           "LNAME": activist.last_name || activist.name
         },
-        "tags": tags()
+        "tags": tags(widget)
       }
 
       if (activist.city) {
-        form.merge_fields['CITY'] = activist.city;
+        body.merge_fields['CITY'] = activist.city;
       }
       if (activist.phone) {
-        form.merge_fields['PHONE'] = activist.phone;
+        body.merge_fields['PHONE'] = activist.phone;
       }
 
-      const response = await client.put({
-        path: `/lists/${listID}/members/${hash()}`,
-        body: form
-      });
+      const path: string = `/lists/${listID}/members/${hash(activist.email)}`;
+      const response = await client.put({ path, body });
       return { updated_at: response.last_changed };
     },
     merge: async (): Promise<any> => {
@@ -84,96 +82,3 @@ export default <T extends Widget, U extends Activist>(args: MailchimpArgs<T, U>)
     }
   }
 }
-
-// class Mailchimp<T extends Widget, U extends Activist> {
-//   protected client: any;
-//   protected activist: any;
-//   protected widget: any;
-//   protected listID: string
-
-//   constructor({ activist, widget }: MailchimpArgs<T, U>) {  
-//     const {
-//       mailchimp_api_key,
-//       mailchimp_list_id
-//     } = widget.block.mobilization.community;
-
-//     this.client = new MailchimpClient(mailchimp_api_key);
-//     this.listID = mailchimp_list_id;
-//     this.activist = activist;
-//     this.widget = widget;
-//   }
-
-//   get tags () {
-//     const { id, kind, block: { mobilization } } = this.widget;
-//     return [
-//       // TAG COMMUNITY
-//       'C' + mobilization.community.id,
-//       // TAG MOBILIZATION
-//       'M' + mobilization.id,
-//       // TAG WIDGET KIND
-//       kind.toUpperCase().substring(0,1) + '' + id
-//     ]
-//   }
-
-//   get hash () {
-//     return crypto
-//       .createHash('md5')
-//       .update(this.activist.email.toLowerCase())
-//       .digest('hex');
-//   }
-
-//   async subscribe (): Promise<any> {
-//     let form: any = {
-//       "email_address": this.activist.email,
-//       "status": "subscribed",
-//       "merge_fields": {
-//         "FNAME": this.activist.first_name || this.activist.name,
-//         "LNAME": this.activist.last_name || this.activist.name
-//       },
-//       "tags": this.tags
-//     }
-
-//     if (this.activist.city) {
-//       form.merge_fields['CITY'] = this.activist.city;
-//     }
-//     if (this.activist.phone) {
-//       form.merge_fields['PHONE'] = this.activist.phone;
-//     }
-//     console.log('form', form);
-
-//     try {
-//       const response = await this.client.put({
-//         path: `/lists/${this.listID}/members/${this.hash}`,
-//         body: form
-//       });
-//       return { updated_at: response.last_changed };
-//     } catch (err) {
-//       console.log('err', err);
-//       throw new Error('Subscribe Error');
-      
-//     }
-//   }
-
-//   async merge_fields (): Promise<any> {
-//     const form: any = {
-//       tag: 'CITY',
-//       name: 'City',
-//       type: 'text',
-//       required: false,
-//       public: true
-//     }
-//     try {
-//       const response = await this.client.post({
-//         path: `/lists/${this.listID}/merge-fields`,
-//         body: form
-//       })
-//       console.log('response', response);
-//       return {}
-//     } catch (err) {
-//       console.log('err', err);
-//       throw new Error('MergeFields Error');
-//     }
-//   }
-// }
-
-// export default Mailchimp;
