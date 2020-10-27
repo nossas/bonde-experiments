be06f1079242a10efc191123e61d068023224abe
Lucas Pirola <lucaspirola@gmail.com>
2020-10-27 11:31:50 -0300

    Remove message sent after any recieved message

--- packages/zap-central-server/index.ts
+++ packages/zap-central-server/index.ts
@@ -171,158 +171,158 @@ async function start(client: Client) {
     //      res.send('csv generated')
   });
 
-  client.onAck((c: any) => console.log(c.id, c.body, c.ack));
+//   client.onAck((c: any) => console.log(c.id, c.body, c.ack));
 
-  client.onAddedToGroup((newGroup) =>
-    console.log("Added to new Group", newGroup.id)
-  );
+//   client.onAddedToGroup((newGroup) =>
+//     console.log("Added to new Group", newGroup.id)
+//   );
 
-  client.onIncomingCall((call) => console.log("newcall", call));
+//   client.onIncomingCall((call) => console.log("newcall", call));
 
-  const prods = await client.getBusinessProfilesProducts(me.wid);
-  console.log(prods);
+//   const prods = await client.getBusinessProfilesProducts(me.wid);
+//   console.log(prods);
 
-  // client.onParticipantsChanged("XXXXXXXX-YYYYYYYY@g.us", (participantChangedEvent:any) => console.log("participant changed for group", participantChangedEvent));
+//   // client.onParticipantsChanged("XXXXXXXX-YYYYYYYY@g.us", (participantChangedEvent:any) => console.log("participant changed for group", participantChangedEvent));
 
-  //Returns 'CONNECTED' or 'TIMEOUT' or 'CONFLICT' (if user opens whatsapp web somewhere else)
-  client.onStateChanged((state) => {
-    console.log("statechanged", state);
-    if (state === "CONFLICT" || state === "UNLAUNCHED") client.forceRefocus();
-  });
+//   //Returns 'CONNECTED' or 'TIMEOUT' or 'CONFLICT' (if user opens whatsapp web somewhere else)
+//   client.onStateChanged((state) => {
+//     console.log("statechanged", state);
+//     if (state === "CONFLICT" || state === "UNLAUNCHED") client.forceRefocus();
+//   });
 
-  // setTimeout(_=> client.kill(), 3000);
+//   // setTimeout(_=> client.kill(), 3000);
 
-  // const allmsgs = await client.loadAndGetAllMessagesInChat('XXXXXXXX-YYYYYYYY@g.us",true,false);
-  // console.log("TCL: start -> allMessages", allmsgs.length);
+//   // const allmsgs = await client.loadAndGetAllMessagesInChat('XXXXXXXX-YYYYYYYY@g.us",true,false);
+//   // console.log("TCL: start -> allMessages", allmsgs.length);
 
-  client.onAnyMessage((message) => {
-    console.log(message.type);
-    if (message.body === "DELETE")
-      client.deleteMessage(message.from, message.id, false);
-  });
-  // client.onParticipantsChanged("XXXXXXXXXX-YYYYYYYYY@g.us",x=>console.log(x))
-  client.onMessage(async (message) => {
-    try {
-      const mp3_message_id = await client.sendAudio(
-        message.from,
-        "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3",
-        null
-      );
-      console.log("start -> mp", mp3_message_id);
+//   client.onAnyMessage((message) => {
+//     console.log(message.type);
+//     if (message.body === "DELETE")
+//       client.deleteMessage(message.from, message.id, false);
+//   });
+//   // client.onParticipantsChanged("XXXXXXXXXX-YYYYYYYYY@g.us",x=>console.log(x))
+//   client.onMessage(async (message) => {
+//     try {
+//       const mp3_message_id = await client.sendAudio(
+//         message.from,
+//         "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3",
+//         null
+//       );
+//       console.log("start -> mp", mp3_message_id);
 
-      const isConnected = await client.isConnected();
-      console.log("TCL: start -> isConnected", isConnected);
-      console.log(message.body, message.id, message?.quotedMsgObj?.id);
-      if (message.mimetype) {
-        const filename = `${message.t}.${mime.extension(message.mimetype)}`;
+//       const isConnected = await client.isConnected();
+//       console.log("TCL: start -> isConnected", isConnected);
+//       console.log(message.body, message.id, message?.quotedMsgObj?.id);
+//       if (message.mimetype) {
+//         const filename = `${message.t}.${mime.extension(message.mimetype)}`;
 
-        // if it is a sticker, you need to run this.
-        let mediaData;
-        if (message.type === "sticker") {
-          //getStickerDecryptable is an insiders feature!
-          // let stickerDecryptable = await client.getStickerDecryptable(message.id);
-          // if(stickerDecryptable) mediaData = await decryptMedia(stickerDecryptable, uaOverride);
-        } else {
-          // mediaData = await decryptMedia(message, uaOverride);
-        }
-        // you can send a file also with sendImage or await client.sendFile
-        await client.sendImage(
-          message.from,
-          `data:${message.mimetype};base64,${mediaData.toString("base64")}`,
-          filename,
-          `You just sent me this ${message.type}`
-        );
+//         // if it is a sticker, you need to run this.
+//         let mediaData;
+//         if (message.type === "sticker") {
+//           //getStickerDecryptable is an insiders feature!
+//           // let stickerDecryptable = await client.getStickerDecryptable(message.id);
+//           // if(stickerDecryptable) mediaData = await decryptMedia(stickerDecryptable, uaOverride);
+//         } else {
+//           // mediaData = await decryptMedia(message, uaOverride);
+//         }
+//         // you can send a file also with sendImage or await client.sendFile
+//         await client.sendImage(
+//           message.from,
+//           `data:${message.mimetype};base64,${mediaData.toString("base64")}`,
+//           filename,
+//           `You just sent me this ${message.type}`
+//         );
 
-        //send the whole data URI so the mimetype can be checked.
-        await client.sendImageAsSticker(
-          message.from,
-          `data:${message.mimetype};base64,${mediaData.toString("base64")}`
-        );
-        //get this numbers products
-        // const products = await client.getBusinessProfilesProducts(message.to);
+//         //send the whole data URI so the mimetype can be checked.
+//         await client.sendImageAsSticker(
+//           message.from,
+//           `data:${message.mimetype};base64,${mediaData.toString("base64")}`
+//         );
+//         //get this numbers products
+//         // const products = await client.getBusinessProfilesProducts(message.to);
 
-        // //send a product from this number to that number
-        //  await client.sendImageWithProduct(
-        //   `data:${message.mimetype};base64,${mediaData.toString('base64')}`,
-        //   message.from,
-        //   'check out this product',
-        //   message.to,
-        //   products[0].id)
+//         // //send a product from this number to that number
+//         //  await client.sendImageWithProduct(
+//         //   `data:${message.mimetype};base64,${mediaData.toString('base64')}`,
+//         //   message.from,
+//         //   'check out this product',
+//         //   message.to,
+//         //   products[0].id)
 
-        // await client.forwardMessages(message.from,message,false);
+//         // await client.forwardMessages(message.from,message,false);
 
-        await client.forwardMessages(message.from, message.id, false);
-        // fs.writeFileSync(filename, mediaData, function(err) {
-        //   if (err) {
-        //     return console.log(err);
-        //   }
-        //   console.log('The file was saved!');
-        // });
+//         await client.forwardMessages(message.from, message.id, false);
+//         // fs.writeFileSync(filename, mediaData, function(err) {
+//         //   if (err) {
+//         //     return console.log(err);
+//         //   }
+//         //   console.log('The file was saved!');
+//         // });
 
-        /**
-         * You can also send the file as a relative file reference. The library will automatically open the file and get the dataUrl
-         */
-        const message_id_from_file = await client.sendImage(
-          message.from,
-          "./" + filename,
-          filename,
-          "from file",
-          null,
-          true,
-          false
-        );
-        console.log("start -> message_id", message_id_from_file);
+//         /**
+//          * You can also send the file as a relative file reference. The library will automatically open the file and get the dataUrl
+//          */
+//         const message_id_from_file = await client.sendImage(
+//           message.from,
+//           "./" + filename,
+//           filename,
+//           "from file",
+//           null,
+//           true,
+//           false
+//         );
+//         console.log("start -> message_id", message_id_from_file);
 
-        /**
-         * Now you can send an animated gif via url
-         */
-        const sticker_from_url_gif_id = await client.sendStickerfromUrl(
-          message.from,
-          "https://i.giphy.com/media/yJil9u57ybQ9movc6E/source.gif"
-        );
-        console.log(
-          "start -> sticker_from_url_gif_id",
-          sticker_from_url_gif_id
-        );
-      } else if (message.type === "location") {
-        if (message.shareDuration)
-          console.log(
-            "This user has started sharing their live location",
-            message.author || message.from
-          );
-        console.log(
-          "TCL: location -> message",
-          message.lat,
-          message.lng,
-          message.loc
-        );
-        await client.sendLocation(
-          message.from,
-          `${message.lat}`,
-          `${message.lng}`,
-          `Youre are at ${message.loc}`
-        );
-      } else {
-        // var sentMessageId = await client.sendText(message.from, message.body);
-        // console.log("start -> sentMessageId", sentMessageId)
-        // //send a giphy gif
-        //   await client.forwardMessages(message.from,message,false);
-        // await client.sendGiphy(message.from,'https://media.giphy.com/media/oYtVHSxngR3lC/giphy.gif','Oh my god it works');
-        // console.log("TCL: start -> message.from,message.body,message.id.toString()", message.from,message.body,message.id.toString())
-        // await client.reply(message.from,message.body,message);
-      }
-    } catch (error) {
-      console.log("TCL: start -> error", error);
-    }
-  });
+//         /**
+//          * Now you can send an animated gif via url
+//          */
+//         const sticker_from_url_gif_id = await client.sendStickerfromUrl(
+//           message.from,
+//           "https://i.giphy.com/media/yJil9u57ybQ9movc6E/source.gif"
+//         );
+//         console.log(
+//           "start -> sticker_from_url_gif_id",
+//           sticker_from_url_gif_id
+//         );
+//       } else if (message.type === "location") {
+//         if (message.shareDuration)
+//           console.log(
+//             "This user has started sharing their live location",
+//             message.author || message.from
+//           );
+//         console.log(
+//           "TCL: location -> message",
+//           message.lat,
+//           message.lng,
+//           message.loc
+//         );
+//         await client.sendLocation(
+//           message.from,
+//           `${message.lat}`,
+//           `${message.lng}`,
+//           `Youre are at ${message.loc}`
+//         );
+//       } else {
+//         // var sentMessageId = await client.sendText(message.from, message.body);
+//         // console.log("start -> sentMessageId", sentMessageId)
+//         // //send a giphy gif
+//         //   await client.forwardMessages(message.from,message,false);
+//         // await client.sendGiphy(message.from,'https://media.giphy.com/media/oYtVHSxngR3lC/giphy.gif','Oh my god it works');
+//         // console.log("TCL: start -> message.from,message.body,message.id.toString()", message.from,message.body,message.id.toString())
+//         // await client.reply(message.from,message.body,message);
+//       }
+//     } catch (error) {
+//       console.log("TCL: start -> error", error);
+//     }
+//   });
 
-  // const groupCreationEvent = await client.createGroup('coolnewgroup','0000000000@c.us');
-  // console.log("start -> groupCreationEvent", groupCreationEvent)
-  //wait a few seconds and make a group
+//   // const groupCreationEvent = await client.createGroup('coolnewgroup','0000000000@c.us');
+//   // console.log("start -> groupCreationEvent", groupCreationEvent)
+//   //wait a few seconds and make a group
 }
 
-//you can now create two sessions pointing
-//two the same message handler
+// //you can now create two sessions pointing
+// //two the same message handler
 
 /**
  * it can be null, which will default to 'session' folder.
